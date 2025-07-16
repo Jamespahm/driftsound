65311072022ad8a7a42be71804363383027014b3
James Pham <hoanggfff@example.com>
2025-07-16 21:55:59 +0700

    feat: Update and expand sound icon library

--- src/data/sounds.jsx
+++ src/data/sounds.jsx
@@ -2,35 +2,13 @@
 
 const sounds = [
     {
-        id: 'rain',
-        name: 'Rain',
-        icon: '/icons/rain.svg',
-        src: '/sounds/rain.mp3',
+        id: 'heavyrain',
+        name: 'Heavyrain',
+        icon: '/icons/rain-heavy.svg',
+        src: '/sounds/rain-heavy.mp3',
         defaultVolume: 0.3
 
     },
-    {
-        id: 'campfire',
-        name: 'Campfire',
-        icon: '/icons/campfire.svg',
-        src: '/sounds/campfire.mp3',
-        defaultVolume: 0.4
-    },
-    {
-        id: 'wind',
-        name: 'Wind',
-        icon: '/icons/wind.svg',
-        src: '/sounds/wind.mp3',
-        defaultVolume: 0.25
-    },
-    {
-        id: 'forest',
-        name: 'Forest',
-        icon: '/icons/forest.svg',
-        src: '/sounds/forest.mp3',
-        defaultVolume: 0.05
-    },
-
     {
         id: 'thunder',
         name: 'Thunder',
@@ -39,9 +17,43 @@ const sounds = [
         defaultVolume: 0.1
     },
     {
+        id: 'lightrain',
+        name: 'Lightrain',
+        icon: '/icons/rain-light.svg',
+        src: '/sounds/rain-light.mp3',
+        defaultVolume: 0.3
+
+    },
+    {
+        id: 'rain-on-tent',
+        name: 'Rain-on-tent',
+        icon: '/icons/rain-on-tent.svg',
+        src: '/sounds/rain-on-tent.mp3',
+        defaultVolume: 0.25
+    },
+    ////////////////////////
+
+
+
+
+    {
+        id: 'wind',
+        name: 'Wind',
+        icon: '/icons/wind.svg',
+        src: '/sounds/wind.mp3',
+        defaultVolume: 0.25
+    },
+    {
+        id: 'howlingwind',
+        name: 'Howlingwind',
+        icon: '/icons/wind-howling.svg',
+        src: '/sounds/wind-howling.mp3',
+        defaultVolume: 0.25
+    },
+    {
         id: 'river',
         name: 'River',
-        icon: '/icons/water-stream.svg',
+        icon: '/icons/river.svg',
         src: '/sounds/river.mp3',
         defaultVolume: 0.2
     },
@@ -53,6 +65,10 @@ const sounds = [
         src: '/sounds/waves.mp3',
         defaultVolume: 0.1
     },
+
+
+    ////////////////////////
+
     {
         id: 'waterfall',
         name: 'Waterfall',
@@ -60,14 +76,12 @@ const sounds = [
         src: '/sounds/waterfall.mp3',
         defaultVolume: 0.1
     },
-
-
     {
-        id: 'rain-on-tent',
-        name: 'Rain-on-tent',
-        icon: '/icons/rain-on-tent.svg',
-        src: '/sounds/rain.mp3',
-        defaultVolume: 0.25
+        id: 'underwater',
+        name: 'Underwater',
+        icon: '/icons/underwater.svg',
+        src: '/sounds/underwater.mp3',
+        defaultVolume: 0.1
     },
     {
         id: 'bubbles',
@@ -77,22 +91,31 @@ const sounds = [
         defaultVolume: 0.05
     },
     {
-        id: 'washing-machine',
-        name: 'Washing-machine',
-        icon: '/icons/washing-machine.svg',
-        src: '/sounds/washing-machine.mp3',
+        id: 'water',
+        name: 'Water',
+        icon: '/icons/water.svg',
+        src: '/sounds/droplets.mp3',
         defaultVolume: 0.1
+    },
+
+
+    ////////////////////////
+
+
+    {
+        id: 'campfire',
+        name: 'Campfire',
+        icon: '/icons/campfire.svg',
+        src: '/sounds/campfire.mp3',
+        defaultVolume: 0.4
     },
     {
-        id: 'train',
-        name: 'Train',
-        icon: '/icons/train.svg',
-        src: '/sounds/train.mp3',
-        defaultVolume: 0.1
+        id: 'forest',
+        name: 'Forest',
+        icon: '/icons/forest.svg',
+        src: '/sounds/forest.mp3',
+        defaultVolume: 0.05
     },
-
-
-
     {
         id: 'leaves',
         name: 'Leaves',
@@ -100,6 +123,89 @@ const sounds = [
         src: '/sounds/leaves.mp3',
         defaultVolume: 0.25
     },
+    {
+        id: 'singingbowl',
+        name: 'Singingbowl',
+        icon: '/icons/singingbowl.svg',
+        src: '/sounds/singingbowl.mp3',
+        defaultVolume: 0.25
+    },
+
+    ////////////////////////
+
+    {
+        id: 'dog',
+        name: 'Dog',
+        icon: '/icons/dog.svg',
+        src: '/sounds/dog.mp3',
+        defaultVolume: 0.05
+    },
+    {
+        id: 'cat',
+        name: 'Cat',
+        icon: '/icons/cat.svg',
+        src: '/sounds/cat.mp3',
+        defaultVolume: 0.05
+    },
+    {
+        id: 'bee',
+        name: 'Bee',
+        icon: '/icons/bee.svg',
+        src: '/sounds/bee.mp3',
+        defaultVolume: 0.05
+    },
+    {
+        id: 'cricket',
+        name: 'cricket',
+        icon: '/icons/cricket.svg',
+        src: '/sounds/cricket.mp3',
+        defaultVolume: 0.05
+    },
+
+
+    ////////////////////////
+
+    {
+        id: 'train',
+        name: 'Train',
+        icon: '/icons/train.svg',
+        src: '/sounds/train.mp3',
+        defaultVolume: 0.1
+    },
+    {
+        id: 'airplane',
+        name: 'Airplane',
+        icon: '/icons/airplane.svg',
+        src: '/sounds/airplane.mp3',
+        defaultVolume: 0.1
+    },
+
+
+    {
+        id: 'road',
+        name: 'Road',
+        icon: '/icons/road.svg',
+        src: '/sounds/road.mp3',
+        defaultVolume: 0.1
+    },
+    {
+        id: 'washing-machine',
+        name: 'Washing-machine',
+        icon: '/icons/washing-machine.svg',
+        src: '/sounds/washing-machine.mp3',
+        defaultVolume: 0.1
+    },
+
+
+    //////////////////////
+    {
+        id: 'fireworks',
+        name: 'fireworks',
+        icon: '/icons/fireworks.svg',
+        src: '/sounds/fireworks.mp3',
+        defaultVolume: 0.05
+    },
+
     {
         id: 'coffee-place',
         name: 'Coffee-place',
@@ -114,129 +220,9 @@ const sounds = [
         src: '/sounds/noise.wav',
         defaultVolume: 0.1
     },
-    {
-        id: 'water',
-        name: 'Water',
-        icon: '/icons/water.svg',
-        src: '/sounds/droplets.mp3',
-        defaultVolume: 0.1
-    },
 
-    {
-        id: 'leaves1',
-        name: 'Leaves',
-        icon: '/icons/leaves.svg',
-        src: '/sounds/leaves.mp3',
-        defaultVolume: 0.25
-    },
-    {
-        id: 'coffee-place1',
-        name: 'Coffee-place',
-        icon: '/icons/coffee-place.svg',
-        src: '/sounds/coffee-place.mp3',
-        defaultVolume: 0.2
-    },
-    {
-        id: 'noise1',
-        name: 'Noise',
-        icon: '/icons/noiseb.svg',
-        src: '/sounds/noise.wav',
-        defaultVolume: 0.1
-    },
-    {
-        id: 'water1',
-        name: 'Water',
-        icon: '/icons/water.svg',
-        src: '/sounds/droplets.mp3',
-        defaultVolume: 0.1
-    },
 
-    {
-        id: 'leaves2',
-        name: 'Leaves',
-        icon: '/icons/leaves.svg',
-        src: '/sounds/leaves.mp3',
-        defaultVolume: 0.25
-    },
-    {
-        id: 'coffee-place2',
-        name: 'Coffee-place',
-        icon: '/icons/coffee-place.svg',
-        src: '/sounds/coffee-place.mp3',
-        defaultVolume: 0.2
-    },
-    {
-        id: 'noise2',
-        name: 'Noise',
-        icon: '/icons/noiseb.svg',
-        src: '/sounds/noise.wav',
-        defaultVolume: 0.1
-    },
-    {
-        id: 'water2',
-        name: 'Water',
-        icon: '/icons/water.svg',
-        src: '/sounds/droplets.mp3',
-        defaultVolume: 0.1
-    },
 
-    {
-        id: 'leaves3',
-        name: 'Leaves',
-        icon: '/icons/leaves.svg',
-        src: '/sounds/leaves.mp3',
-        defaultVolume: 0.25
-    },
-    {
-        id: 'coffee-place3',
-        name: 'Coffee-place',
-        icon: '/icons/coffee-place.svg',
-        src: '/sounds/coffee-place.mp3',
-        defaultVolume: 0.2
-    },
-    {
-        id: 'noise3',
-        name: 'Noise',
-        icon: '/icons/noiseb.svg',
-        src: '/sounds/noise.wav',
-        defaultVolume: 0.1
-    },
-    {
-        id: 'water3',
-        name: 'Water',
-        icon: '/icons/water.svg',
-        src: '/sounds/droplets.mp3',
-        defaultVolume: 0.1
-    },
-
-    {
-        id: 'leaves4',
-        name: 'Leaves',
-        icon: '/icons/leaves.svg',
-        src: '/sounds/leaves.mp3',
-        defaultVolume: 0.25
-    },
-    {
-        id: 'coffee-place4',
-        name: 'Coffee-place',
-        icon: '/icons/coffee-place.svg',
-        src: '/sounds/coffee-place.mp3',
-        defaultVolume: 0.2
-    },
-    {
-        id: 'noise4',
-        name: 'Noise',
-        icon: '/icons/noiseb.svg',
-        src: '/sounds/noise.wav',
-        defaultVolume: 0.1
-    },
-    {
-        id: 'water4',
-        name: 'Water',
-        icon: '/icons/water.svg',
-        src: '/sounds/droplets.mp3',
-        defaultVolume: 0.1
-    },
 ];
 
 export default sounds;
